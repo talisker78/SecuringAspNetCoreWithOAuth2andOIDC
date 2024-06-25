0d50d8387e1b75335601ddd3ba8115f60ccfcdbe
talisker78 <jimwilde@gmail.com>
2024-06-24 21:27:45 -0400

    Attempting to get the eff back to where we were.

--- Starter files/ImageGallery.Client/Program.cs
+++ Starter files/ImageGallery.Client/Program.cs
@@ -1,15 +1,99 @@
+using ImageGallery.API.Authorization;
+using ImageGallery.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(configure =>
+        configure.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+JsonWebTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+builder.Services.AddAccessTokenManagement();
+
+// create an HttpClient used for accessing the API
+builder.Services.AddHttpClient("APIClient", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ImageGalleryAPIRoot"]);
+    client.DefaultRequestHeaders.Clear();
+    client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+}).AddUserAccessTokenHandler();
+
+builder.Services.AddHttpClient("IDPClient", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:5001");
+});
+
+
+
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    }).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+    {
+        options.AccessDeniedPath = "/Authentication/AccessDenied";
+    })
+    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+    {
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.Authority = builder.Configuration["IDPBaseAddress"];
+        options.ClientId = "imagegalleryclient";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+        //options.UsePkce = true;
+        //options.Scope.Add("openid");
+        //options.Scope.Add("profile");
+        //options.CallbackPath = new PathString("signin-oidc");
+        options.SaveTokens = true;
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.ClaimActions.Remove("aud");
+        options.ClaimActions.DeleteClaim("sid");
+        options.ClaimActions.DeleteClaim("idp");
+        options.Scope.Add("roles");
+        options.Scope.Add("imagegalleryapi.read");
+        options.Scope.Add("imagegalleryapi.write");
+        options.Scope.Add("country");
+        options.Scope.Add("offline_access");
+        options.ClaimActions.MapJsonKey("role", "role");
+        options.ClaimActions.MapUniqueJsonKey("country", "country");
+        options.TokenValidationParameters = new()
+        {
+            NameClaimType = "name",
+            RoleClaimType = "role"
+        };
+    });
+
+builder.Services.AddAuthorization(authorizationOptions =>
+{
+    authorizationOptions.AddPolicy("UserCanAddImage",
+        AuthorizationPolicies.CanAddImage());
+
+    authorizationOptions.AddPolicy("ClientApplicationCanWrite", policyBuilder =>
+    {
+        policyBuilder.RequireClaim("scope", "imagegalleryapi.write");
+    });
+
+    authorizationOptions.AddPolicy("MustOwnImage", policyBuilder =>
+    {
+        policyBuilder.RequireAuthenticatedUser();
+        policyBuilder.AddRequirements(new MustOwnImageRequirement());
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler();
     app.UseHsts();
 }
 
@@ -18,10 +102,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Gallery}/{action=Index}/{id?}");
 
 app.Run();
